1a201dc2b2b9bce0c1e2f958d27cdea5afd73823
Jackson Kiarie <jacknkiarie@gmail.com>
2023-11-30 17:53:45 +0300

    add specs to users controller

---
+++ app/services/api_response.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class ApiResponse
+
+  def self.render(...)
+    new(...).call
+  end
+
+  attr_accessor :message, :data, :errors
+
+  def initialize(message:, data: nil, errors: nil)
+    @message = message
+    @data = data
+    @errors = errors
+  end
+
+  def call
+    {
+      message: @message,
+      data: @data,
+      errors: @errors
+    }.to_json
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,8 +6,8 @@ require_relative '../config/environment'
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-require_relative 'support/factory_bot'
-require_relative 'support/shoulda_matchers'
+Dir[Rails.root.join('spec/support/**/*.rb')].sort.each { |f| require f }
+
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are

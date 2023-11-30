a6bcee27ad9a672aa41f3adbaba8f4a659a2bb78
Jackson Kiarie <jacknkiarie@gmail.com>
2023-11-30 14:58:05 +0300

    allow user to set their password during account creation

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,5 +5,7 @@ FactoryBot.define do
     name { Faker::Name.name }
     email { Faker::Internet.email }
     phone { Faker::PhoneNumber.phone_number }
+    password { Faker::Alphanumeric.alphanumeric(number: 10) }
+    password_confirmation { password }
   end
 end

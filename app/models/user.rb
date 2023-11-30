73ba74605c96d1f8840b524ce657ef3b9795fa2a
Jackson Kiarie <jacknkiarie@gmail.com>
2023-11-30 14:41:16 +0300

    add test and model validation to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :phone, presence: true, uniqueness: true
 end

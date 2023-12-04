c230f1d68cef651f3951e3cefef4b27afef2da67
Jackson Kiarie <jacknkiarie@gmail.com>
2023-12-04 18:48:36 +0300

    hide password diget from json response

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,14 +4,14 @@ class Api::V1::UsersController < ApplicationController
   skip_before_action :authenticate_token!, only: [:create]
 
   def show
-    render json: ApiResponse.render(data: current_user), status: :ok
+    render json: ApiResponse.render(data: current_user.as_json(except: :password_digest)), status: :ok
   end
 
   def create
     @user = User.new(user_params)
 
     if @user.save
-      render json: ApiResponse.render(message: 'User has been created successfully', data: @user), status: :created
+      render json: ApiResponse.render(message: 'User has been created successfully', data: @user.as_json(except: :password_digest)), status: :created
     else
       render json: ApiResponse.render(message: 'We were unable to create your account', errors: @user.errors), status: :unprocessable_entity
     end

42174b0fc14cb1772e206ebb3c55610cdb09c6cc
Jackson Kiarie <jacknkiarie@gmail.com>
2023-12-04 18:17:00 +0300

    create endpoint to request for transaction reports which are sent to the user via email

--- app/models/wallet_transaction.rb
+++ app/models/wallet_transaction.rb
@@ -7,6 +7,8 @@ class WalletTransaction < ApplicationRecord
   validates :transaction_type, presence: true
   validates :amount, numericality: { greater_than_or_equal_to: 5 }
 
+  scope :created_between, lambda { |start_date, end_date| where(created_at: start_date.beginning_of_day..end_date.end_of_day) }
+
   enum transaction_type: {
     credit: 0,
     debit: 1
